6e3f3c989d168c31b6979e981c5bdf3f6aa009c7
Chiranjibi Biswal <99603408+ChiranjibiBiswal@users.noreply.github.com>
2023-09-11 20:23:49 +0000

    convertion of datatypes

---
+++ basics/convertionOperation.js
@@ -0,0 +1,20 @@
+let score = false
+
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+    "33" => 33
+    "33abc" => NaN
+    true => 1 , false => 0
+*/
+
+let isLoggedIn = 0
+let booleanNumber = Boolean(isLoggedIn)
+
+console.log(typeof booleanNumber)
+console.log(booleanNumber)
