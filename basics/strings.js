7950fbdc5993cba1d1e2756b2e95a00e100aae87
Chiranjibi Biswal <99603408+ChiranjibiBiswal@users.noreply.github.com>
2023-10-31 18:11:30 +0000

    string done

---
+++ basics/strings.js
@@ -0,0 +1,19 @@
+const name = new String("Hitesh")
+// const number = 40
+
+// console.log(`${name} ${number}`)
+// console.log(name.__proto__);
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.charAt(2))
+// console.log(name.indexOf("t"))
+
+// const newString = name.substring(0, 3)
+// console.log(newString);
+
+// const newName = name.slice(-5, 4)
+// console.log(newName)
+
+let user = " Chira-njibi "
+console.log(user.includes("jibi"))
+console.log(user.trim())
