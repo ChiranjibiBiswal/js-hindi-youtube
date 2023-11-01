cfff100a22a630d6ac3fbc1b5fa0ba93aa47bd81
Chiranjibi Biswal <99603408+ChiranjibiBiswal@users.noreply.github.com>
2023-11-01 14:07:21 +0000

    Nums and maths

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,19 @@
+//Numbers
+
+const num = 9054.9876
+
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(2))
+const hundreds = 100000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+//Maths
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(90.998))
+
+// console.log(Math.random()*5)
+const max = 20
+const min = 10
+console.log(Math.round(Math.random() * (max - min)) + min)
