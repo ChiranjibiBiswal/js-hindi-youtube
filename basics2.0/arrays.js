9d186e941d0b3ef9cf6a099d22823a9197802b7a
Chiranjibi Biswal <99603408+ChiranjibiBiswal@users.noreply.github.com>
2023-11-03 14:23:03 +0000

    arrays done

---
+++ basics2.0/arrays.js
@@ -0,0 +1,24 @@
+const myName = ["Arya", "Chiranjibi" ,"Saktiman", "Superman"]
+const newArray = new Array("SpiderMan" ,"Captain America", "Thor", "Hulk")
+
+// newArray.push("Vision")
+// newArray.push("Black Panther")
+// newArray.pop()
+
+// newArray.unshift("Rakesh")
+// newArray.shift()
+// console.log(newArray.includes("Rakesh"))
+// console.log(newArray.indexOf("Rakesh"))
+
+// const myArray = newArray.join()
+
+// console.log(newArray)
+// console.log(typeof myArray)
+
+const number = [0, 1, 2, 3, 4, 5]
+const num1 = number.slice(0, 3)
+console.log(number)
+console.log(num1)
+const num2 = number.splice(0,4)
+console.log(number)
+console.log(num2)
