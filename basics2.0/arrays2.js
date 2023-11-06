c6e9737a37d6eea56a0c072b70fe80c820585373
Chiranjibi Biswal <99603408+ChiranjibiBiswal@users.noreply.github.com>
2023-11-06 14:07:49 +0000

    array2 done

---
+++ basics2.0/arrays2.js
@@ -0,0 +1,19 @@
+const marvel_heros = ["Thor" ,"Iron Man" ,"Spider Man"]
+const dc_heros = ["Super Man", "Flash", "Bat Man"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros[3][1])
+
+// const allHeros = marvel_heros.concat(dc_heros)
+
+const another_number = [1, 2, 3, [4, 5, 6], 7, [6, 7 ,[4, 5]]]
+
+// console.log(another_number.flat(Infinity))
+
+// console.log(Array.from({name: "Hitesh"}))
+
+// console.log(Array.of(marvel_heros, dc_heros))
+// console.log(Array.isArray(marvel_heros))
+
+// console.log(Array.isArray(90))
